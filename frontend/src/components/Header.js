b07350a7dcb530b2f88c3e143e8b1064beb75589
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-06-22 21:17:23 +0200

    Added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
+import {BsCartFill} from 'react-icons/bs';
+import {FaUserAlt} from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <header>
+    <header className='fixed shadow-md w-full h-16 px-2 md:px-4'>
         {/* Desktop */}
-        <div className=''>
-            <img src={Logo} alt='logo' />
+        <div className=' flex items-center h-full justify-between'>
+            <Link to={" "}>
+                <div className='h-12'>
+                    <img src={Logo} alt='logo' className='h-full' />
+                </div>
+            </Link>
+
+            <div className='flex items-center gap-4 md:gap-7'>
+                <nav className='flex gap-4 md:gap-6 text-base md:text-lg'>
+                    <Link to={" "}>Home</Link>
+                    <Link to={"menu"}>Menu</Link>
+                    <Link to={"about"}>About</Link>
+                    <Link to={"contact"}>Contact</Link>
+                </nav>
+
+                <div className=''>
+                    <BsCartFill/>
+                </div>
+                
+                <div className=''>
+                    <FaUserAlt/>
+                </div>
+            </div>
         </div>
         {/* Mobile */}
     </header>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter, CreateRoutesFromElement, createRoutesFromElements, Route, RouteProvider, RouterProvider} from 'react-router-dom';
+
+
+const router = createBrowserRouter (
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
