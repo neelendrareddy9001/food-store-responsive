6f127b462cb5ca13553be7ca3eee0c73d03928f9
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-08-10 23:49:35 +0200

    Added new product

--- frontend/src/components/NewProduct.js
+++ frontend/src/components/NewProduct.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const NewProduct = () => {
   return (
-    <div>NewProduct</div>
+    <div>
+      <form>
+        <label htmlFor='name'>Name</label>
+        <input type={"text"} name='name' />
+
+        <select>
+          <option>Fruits</option>
+          <option>Vegetable</option>
+          <option>Icream</option>
+          <option>Dosa</option>
+          <option>Pizza</option>
+        </select>
+      </form>
+    </div>
   )
 }
 
